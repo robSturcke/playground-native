0fbc124c2ac31d35b5c001bfec698d1f7dc1e272
Rob Sturcke <rob.emil.sturcke@gmail.com>
2023-01-29 11:31:47 -0500

    go to first screen

--- src/stacks/about.tsx
+++ src/stacks/about.tsx
@@ -9,6 +9,10 @@ function About({ navigation }) {
       />
       <Button title='Go to Home' onPress={() => navigation.navigate('Home')} />
       <Button title='Go back' onPress={() => navigation.goBack()} />
+      <Button
+        title='Go back to first screen in stack'
+        onPress={() => navigation.popToTop()}
+      />
     </View>
   );
 }
